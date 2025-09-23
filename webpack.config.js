0d508f5fb823e57e1da19144390d8614a7a09c25
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-23 15:46:27 -0700

    placed devServer outside of output. works clean

--- webpack.config.js
+++ webpack.config.js
@@ -9,17 +9,18 @@ module.exports={
     output: {
         path: path.resolve(__dirname,"dist"),
         filename: "[name][contenthash].js",
-        devServer: {
-            static: {
-                directory: path.resolve(__dirname,"dist")
-            },
-            port:3000,
-            open:true,
-            hot:true,
-            compress:true,
-            historyApiFallback:true
+        
+        clean:true,
+    },
+    devServer: {
+        static: {
+            directory: path.resolve(__dirname,"dist")
         },
-        clean:true
+        port:3000,
+        open:true,
+        hot:true,
+        compress:true,
+        historyApiFallback:true
     },
     module:{
         rules:[
