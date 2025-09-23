2f60cf0d6592fca5298aa2d9ea1fb136e161b388
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-23 15:43:33 -0700

    created devServer, command 'dev'

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,16 @@ module.exports={
     output: {
         path: path.resolve(__dirname,"dist"),
         filename: "[name][contenthash].js",
+        devServer: {
+            static: {
+                directory: path.resolve(__dirname,"dist")
+            },
+            port:3000,
+            open:true,
+            hot:true,
+            compress:true,
+            historyApiFallback:true
+        },
         clean:true
     },
     module:{
