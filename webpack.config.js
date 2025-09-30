4109534a508a56ba7ebee82f2e8b0ce4e0d7c66b
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-09-30 09:08:56 -0700

    remove babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -30,16 +30,6 @@ module.exports={
                 use: ["style-loader", "css-loader", "sass-loader"],
             },
             {
-                test:/\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                    options: {
-                        presets: ["@babel/preset-env"],
-                    }
-                }
-            },
-            {
                 test:/\.(png|svg|jpg|jpeg|gif)$/i,
                 type:'asset/resource'
             }
