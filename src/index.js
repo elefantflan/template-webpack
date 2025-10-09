12474d92716280b9b4ddc973716e8eccb9b7a52d
elefantflan <214541229+elefantflan@users.noreply.github.com>
2025-10-09 12:47:40 -0700

    simplified index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,3 @@
 import "./styles/main.scss";
 
 const body = document.body;
-const page = document.createElement("div");
-const header = document.createElement("div");
-const main = document.createElement("div");
-const footer = document.createElement("div");
-
-body.append(page);
-page.append(header);
-page.append(main);
-page.append(footer);
